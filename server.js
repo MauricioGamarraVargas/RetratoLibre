547211ec073dc41c8ef277267370076d42ff31b9
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2020-03-29 13:15:25 -0400

    cambios server.js

--- server.js
+++ server.js
@@ -13,7 +13,5 @@ app.get('/contact', (req, res) =>{
     res.sendFile(__dirname + '/contact.html');
 })
 
-app.get('/profile/:name', (req, res) => {
-    res.render('profile', {person: req.params.name});
-})
+
 app.listen(3000);
