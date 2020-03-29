d7a12ab67dce38628550704e60040fa2b0fa67d0
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2020-03-29 13:14:21 -0400

    routing

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
 app.use('/static', express.static('public'));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/index.html'))
+    
 });
 
+app.get('/contact', (req, res) =>{
+    res.sendFile(__dirname + '/contact.html');
+})
+
+app.get('/profile/:name', (req, res) => {
+    res.render('profile', {person: req.params.name});
+})
 app.listen(3000);
