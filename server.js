b6f7ec27dfc20929ddd679b84857b88bdbfdd085
MauricioGamarraVargas <46828503+MauricioGamarraVargas@users.noreply.github.com>
2020-03-31 01:16:15 -0400

    contactanos

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const path = require('path');
 const app = express();
 
 app.use('/static', express.static('public'));
+//Route index or home page
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname + '/index.html'))
     
 });
-
+//Route contact page
 app.get('/contact', (req, res) =>{
     res.sendFile(__dirname + '/contact.html');
+})
+
+app.get('/menu', (req, res) =>{
+    res.sendfile(path.join(__dirname + '/menu.html'));
 })
 
 
